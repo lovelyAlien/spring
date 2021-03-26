efe055125e47619b608b5fa9b250a79d49a96b3d
Jaeseung Choun <54395509+lovelyAlien@users.noreply.github.com>
2021-03-27 00:22:35 +0900

    1주차 수업

---
+++ springcore/src/main/java/com/sparta/springcore/ProductController.java
@@ -0,0 +1,42 @@
+package com.sparta.springcore;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController // JSON으로 데이터를 주고받음을 선언합니다.
+public class ProductController {
+    // 멤버 변수 선언
+    private final ProductService productService;
+
+    // 생성자: ProductController() 가 생성될 때 호출됨
+    @Autowired
+    public ProductController(ProductService productService) {
+        // 멤버 변수 생성
+        this.productService = productService;
+    }
+
+    // 등록된 전체 상품 목록 조회
+    @GetMapping("/api/products")
+    public List<Product> getProducts() throws SQLException {
+        List<Product> products = productService.getProducts();
+        // 응답 보내기
+        return products;
+    }
+
+    // 신규 상품 등록
+    @PostMapping("/api/products")
+    public Product createProduct(@RequestBody ProductRequestDto requestDto) throws SQLException {
+        Product product = productService.createProduct(requestDto);
+        // 응답 보내기
+        return product;
+    }
+
+    // 설정 가격 변경
+    @PutMapping("/api/products/{id}")
+    public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) throws SQLException {
+        Product product = productService.updateProduct(id, requestDto);
+        return product.getId();
+    }
+}
