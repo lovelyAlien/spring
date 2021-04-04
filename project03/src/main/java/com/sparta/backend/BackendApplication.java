014e8d14ff20a2b00ac934b17cc04c2cefe84d28
lovelyAlien <54395509+lovelyAlien@users.noreply.github.com>
2021-04-04 21:54:38 +0900

    update

--- project03/src/main/java/com/sparta/backend/BackendApplication.java
+++ project03/src/main/java/com/sparta/backend/BackendApplication.java
@@ -1,10 +1,12 @@
 package com.sparta.backend;
 
+import com.sparta.backend.selenium.Selenium;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
 import java.util.TimeZone;
 
 @EnableJpaAuditing
@@ -16,7 +18,10 @@ public class    BackendApplication {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)throws IOException {
+        Selenium selenium = new Selenium();
+        selenium.setURL("https://www.stussy.co.kr/collections/tees");
+        selenium.crawling();
         SpringApplication.run(BackendApplication.class, args);
     }
 
